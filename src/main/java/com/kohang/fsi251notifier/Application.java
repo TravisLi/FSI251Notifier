e414b17dbfb561b30aa9f6bf1e52e5467fc0ab9b
Travis Li <coolki@gmail.com>
2022-03-02 19:47:35 +0800

    update dockerFile

--- src/main/java/com/kohang/fsi251notifier/Application.java
+++ src/main/java/com/kohang/fsi251notifier/Application.java
@@ -2,7 +2,6 @@ package com.kohang.fsi251notifier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -12,31 +11,9 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class Application {
 	
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);	
 		
-		//FSI251Recognizer recognizer = ctx.getBean(FSI251Recognizer.class);
-		
-		//recognizer.run();
-		
-		/*FSI251Repository repository = ctx.getBean(FSI251Repository.class);
-		
-		final String SAMPLE_CERT_NO = "A 7956010";
-		
-		FSI251Data data = new FSI251Data();
-		data.setCertNo(SAMPLE_CERT_NO);
-		data.setFileName("testing");
-		data.setCertDate("9/12/2021");
-		
-		repository.save(data);*/
-		
-		//FileAccesser fa = ctx.getBean(FileAccesser.class);
-		
-		//fa.copyAndDeleteFiles(fa.getSrcFiles());
-		
-		/*File document = new File("\\\\TravisNAS\\KoHangNAS\\KoHang\\11 FSI 251\\高衡 FS251 2021\\A7426356 大眾教室(屯門栢麗廣場) 1樓107室.pdf");
-		
-		List<FSI251Data> resultList = recognizer.run(document);*/
-		
+		SpringApplication.run(Application.class, args);	
+				
 	}
 
 }
